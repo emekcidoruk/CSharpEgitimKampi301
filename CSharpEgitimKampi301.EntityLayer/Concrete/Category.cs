4d2ad4708d3c82c26eb4ad8f58fa71ee9531bdd6
Doruk Emekçi <emekcidoruk65@gmail.com>
2025-02-28 11:50:33 +0300

    Add files via upload

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+
+        public int CategoryID { get; set; } // Birincil anahtar olduğunu ve ototmatik artan olduğunu bildirmek adına sınıfın ismi birebir aynı olmalı, sonuna ID getirilmeli
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; } // Bir category'nin içinde birden fazla ürün olabileceği için "Products" yazdık
+
+
+    }
+}
+/*
+ 
+ Field-Variable-Property
+
+----------------------------------
+
+int x; --> Field
+
+----------------------------------
+
+int y { get; set; } --> Property,
+
+----------------------------------
+
+void test()
+{
+    int z; --> Verable
+}
+ 
+ */
